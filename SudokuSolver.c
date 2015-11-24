e225e528bb392451101d257965a590fe683c5fcb
radubb <radubb@users.noreply.github.com>
2015-11-24 13:46:44 +0200

    Calling solver using multiple boards

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -2,6 +2,7 @@
 #include <omp.h>
 
 #define SZ 12
+#define size 2
 
 int isAvailable(int puzzle[][SZ], int row, int col, int num)
 {
@@ -18,7 +19,7 @@ int isAvailable(int puzzle[][SZ], int row, int col, int num)
     return 1;
 }
 
-int fillSudoku(int puzzle[][SZ], int row, int col)
+int fillSudoku(int puzzle[SZ][SZ], int row, int col)
 {
     int i;
     if(row<SZ && col<SZ)
@@ -57,9 +58,11 @@ int fillSudoku(int puzzle[][SZ], int row, int col)
 
 int main()
 {
-    int i, j;
+    int i, j, k;
+	
 
-	int puzzle[SZ][SZ]={{0, 0, 3, 6, 0, 0, 0, 0, 0, 7, 10, 0},
+	int puzzle[5][SZ][SZ]={
+					{{0, 0, 3, 6, 0, 0, 0, 0, 0, 7, 10, 0},
     					{0, 0, 9, 0, 0, 5, 12, 0, 1, 0, 0, 0},
     					{0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5},
     					{7, 0, 5, 0, 0, 0, 8, 6, 0, 4, 3, 0},
@@ -70,20 +73,48 @@ int main()
     					{0, 12, 0, 0, 2, 0, 0, 0, 0, 6, 0, 7},
     					{8, 0, 12, 2, 4, 0, 0, 0, 0, 1, 0, 20},
     					{4, 0, 0, 1, 0, 0, 0 ,2, 11, 12, 9, 0},
-    					{0, 3, 10, 0, 12, 0, 9, 0, 8, 2, 0, 0},
-                        };
+    					{0, 3, 10, 0, 12, 0, 9, 0, 8, 2, 0, 0}},
 
-    if(fillSudoku(puzzle, 0, 0))
-    {
-        printf("\n+-----+-----+-----+\n");
-        for(i=1; i<SZ+1; ++i)
-        {
-            for(j=1; j<SZ+1; ++j) printf("|%d", puzzle[i-1][j-1]);
-            printf("|\n");
-            if (i%3 == 0) printf("+-----+-----+-----+\n");
-        }
-    }
-    else printf("\n\nNO SOLUTION\n\n");
+					{{0, 0, 3, 6, 0, 0, 0, 0, 0, 7, 10, 0},
+    					{0, 0, 9, 0, 0, 5, 12, 0, 1, 0, 0, 0},
+    					{0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 5},
+    					{7, 0, 5, 0, 0, 0, 8, 6, 0, 4, 3, 0},
+    					{6, 1, 0, 0, 0, 9, 0, 0, 12, 0, 0, 11},
+    					{3, 0, 8, 9, 0, 0, 11, 0, 7, 5, 0, 6},
+    					{10, 0, 0, 0, 0, 1, 0, 0, 4, 0, 0, 0},
+    					{9, 0, 0, 8, 0, 7, 4, 0, 0, 0, 5, 0},
+    					{0, 12, 0, 0, 2, 0, 0, 0, 0, 6, 0, 7},
+    					{8, 0, 12, 2, 4, 0, 0, 0, 0, 1, 0, 0},
+    					{4, 0, 0, 1, 0, 0, 0 ,2, 11, 12, 9, 0},
+    					{0, 3, 10, 0, 12, 0, 9, 0, 8, 2, 0, 0}}
+						
+						};
+
+	#pragma omp parallel num_threads(2)
+	{
+		#pragma omp single private(k)
+		{
+			for(k = 0; k < 2; k++) 
+			{
+				#pragma omp task
+				{
+
+					if(fillSudoku(puzzle[k], 0, 0))
+					{
+						printf("\n+-----+-----+-----+\n");
+						for(i=1; i<SZ+1; ++i)
+						{
+							for(j=1; j<SZ+1; ++j) printf("|%d", puzzle[k][i-1][j-1]);
+							printf("|\n");
+							if (i%3 == 0) printf("+-----+-----+-----+\n");
+						}
+					}
+					else printf("\n\nNO SOLUTION\n\n");
+				}
+			}
+		}
+		
+	}
 
     return 0;
 }
